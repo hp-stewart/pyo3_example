256c94880283e3cbec119e18c1836fd604403bfe
Hanna Stewart <hanna.stewart@mohawkcollege.ca>
2023-02-21 11:05:03 -0500

    updated comments

--- call_python_inside_rust/examples/loop7d.rs
+++ call_python_inside_rust/examples/loop7d.rs
@@ -1,25 +1,20 @@
 /*
-This example runs function 7c in a loop so you can quickly try out different inputs and see the result
+This example runs function 7d in a loop so you can quickly try out different inputs and see the result
 
 Expected behavior:
-- Prompt the user to type a color
-- call the function from example 7c in lib, while supplying the users text input 
-- inside the example 7c function, the pyo3 crate is used to create a Python session inside Rust
-    - The return value of this function is in the form Result<Option<char>, Error>
+- call the function from example 7d in lib
+- inside the example 7d function, the pyo3 crate is used to create a Python session inside Rust
+    - The return value of this function is in the form Result<Option<i32>, Error>
     - the Python Module is created from the contents of py/functions_venv.py file
-    - the Python function "def color_emoji()" is loaded from the PyModule
-    - color_emoji() is executed using the users text input
-        - an error is raised if the text input contains a number
-        - the supplied color name is converted into the appropriate CLDR shortname to display a colored circle
-            - ex: "Red" into ":red_circle:"
-        - attempt to convert the CLDR shortname into an emoji using the python emoji module
-            - If the CLDR shortname matches a known emoji, the operation completes and color_emoji() returns Some(emoji)
-            - If the CLDR shortname does not match any emoji, the operation completes and color_emoji() returns None  
+    - the Python function "def random_number()" is loaded from the PyModule
+    - random_number() is executed using the range 1, 10
+        - a random number from 1 to 10 is generated and returned  
     - the rust example function receives a Result containing either OK(PyAny) (if the operation completed) or else PyErr
-    - this result is converted into Rust-compatible types and then returned to main()
-        - If OK: The PyAny is a python Optional which can manually converted to Rust Option Enum and returned inside Rust Result Enum
-        - If PyErr: convert into Rust Err and return inside Rust Result Enum
-    - main() receives the Result<Option<char>,Error> returned by the example fnuction and displays the results
+        - If OK: The PyAny is converted into Rust i32 and returned inside an Option
+            - The option is Some if the number is Odd and None if the number is even
+        - If PyErr: convert into corresponding Rust Err and return inside Rust Result Enum
+    - Some simple arithmatic is performed on the value inside the Option (if it is not none) and it is returned to main() inside a Result 
+    - main() receives the Result<Option<int>,Error> returned by the example fnuction and displays the results
     - the user is prompted to either try again or quit
     - based on user input, either the loop restarts or ends
  */
